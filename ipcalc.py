427877f7d9376c60938f59fd1b55c0a3eefaf5c0
Timothy Pulliam <timothy@Whitey-Senior-2.local>
2017-09-13 14:46:48 -0400

    About to do major refactor

--- ipcalc.py
+++ ipcalc.py
@@ -24,8 +24,10 @@ def ipcalc(argv):
         print_usage()
         return 0
 
-    # Parse the options and arguments
-    command = argv[1]
+
+    if is_ip(argv[1]):
+        ip = argv[1]
+        print("Decimal Notation: " + ipv)
 
     if command == 'b2d':
         print(b2d(argv[2]))
@@ -36,7 +38,45 @@ def ipcalc(argv):
     else:
         print("Invalid Command: Use 'ipcalc help' for usage.")
 
-def parse_ip(ip):
+
+def ip_info(ip):
+    print("Decimal Notation: " + ip_decimal(ip))
+    print("Binary Notation: " + ip_binary(ip))
+    print("Class: " + ipv4_to_class(ip))
+    print("Netmask: ")    
+
+def ip_d2b(ip):
+    """Convert IP address from decimal form to unsigned binary form"""
+    if is_ip(ip) and IPV4_IS_DECIMAL:
+        ip_binary = '.'.join([d2b(octet) for octet in ip.split('.')])
+        return ip_binary
+    else:
+        print("Not a valid IP address")
+        return 1
+
+def ip_decimal(ip):
+    """Return IP address in decimal form"""
+    if is_ip(ip):
+        if IPV4_IS_DECIMAL:
+            return ip
+        else:
+            # Must be in binary form
+            ip_decimal = '.'.join([b2d(octet) for octet in ip.split('.')])
+    else:
+        print("Not a valid IP Address")
+
+def ip_binary(ip):
+    """Return IP address in decimal form"""
+    if is_ip(ip):
+        if IPV4_IS_BINARY:
+            return ip
+        else:
+            # Must be in decimal form
+            ip_decimal = '.'.join([d2b(octet) for octet in ip.split('.')])
+
+
+
+def is_ip(ip):
     """Determine whehter the given IP address is in a valid
     decimal or binary format."""
     global IPV4_IS_DECIMAL
@@ -48,32 +88,46 @@ def parse_ip(ip):
     if re.fullmatch(ipv4_decimal_pattern, ip):
         IPV4_IS_DECIMAL = True
         IPV4_IS_BINARY = False
+        return True
     elif re.fullmatch(ipv4_binary_pattern, ip):
         IPV4_IS_DECIMAL = False
         IPV4_IS_BINARY = True
+        return True
     else:
-        print("Invalid IPv4 Address Format")
-
-
-
-def print_usage():
-    print("""
-    Invalid command. Below are the commands ipcalc accepts
-
-    b2d -- Convert a number in unsigned binary form to decimal form
-    d2b -- Convert a number in decimal form to unsigned binary form
-    ipv4_to_class -- Return the Class of the given IP address
-    help -- Print this help menu
-    """)
+        #print("Invalid IPv4 Address Format")
+        return False
 
 def b2d(binary_string):
     """Convert a number in unsigned binary form to decimal form"""
     return int(binary_string, 2)
 
+
 def d2b(decimal_string):
     """Convert a number in decimal form to unsigned binary form"""
     decimal_int = int(decimal_string)
     return '{:0>8b}'.format(decimal_int)
+
+
+def ip_d2b(ip):
+    """Convert IP address from decimal form to unsigned binary form"""
+    if is_ip(ip) and IPV4_IS_DECIMAL:
+        ip_binary = '.'.join([d2b(octet) for octet in ip.split('.')])
+        return ip_binary
+    else:
+        print("Not a valid IP address")
+        return 1
+
+
+
+def ip_b2d(ip):
+    """Convert IP address from unsigned binary form to decimal form"""
+    if is_ip(ip) and IPV4_IS_BINARY:
+        ip_decimal = '.'.join([b2d(octet) for octet in ip.split('.')])
+        return ip_decimal
+    else:
+        print("Not a valid IP address")
+        return 1
+
 
 def ipv4_to_class(ipv4):
     """"
@@ -90,27 +144,48 @@ def ipv4_to_class(ipv4):
     octets = ipv4.split('.')
     if IPV4_IS_BINARY:
         if 0 <= b2d(octets[0]) < 127:
-            print("Class A")
+            print("A")
         elif 128 <= b2d(octets[0]) <= 191:
-            print("Class B")
+            print("B")
         elif 192 <= b2d(octets[0]) <= 223:
-            print("Class C")
+            print("C")
         elif 224 <= b2d(octets[0]) <= 239:
-            print("Class D (Multicast)")
+            print("D (Multicast)")
         else:
-            print("Class E (Experimental)")
+            print("E (Experimental)")
     else:
         leading_octet = int(octets[0])
         if 0 <= leading_octet < 127:
-            print("Class A")
+            print("A")
         elif 128 <= leading_octet <= 191:
-            print("Class B")
+            print("B")
         elif 192 <= leading_octet <= 223:
-            print("Class C")
+            print("C")
         elif 224 <= leading_octet <= 239:
-            print("Class D (Multicast)")
+            print("D (Multicast)")
         else:
-            print("Class E (Experimental)")
+            print("E (Experimental)")
+
+
+def print_usage():
+    print("""
+    NAME
+        ipcalc - IPv4 Addressing and subnetting calculator.
+    
+    SYNOPSIS
+        ipcalc - [ip_address|command] [options]
+
+    COMMANDS
+        help
+            Print this help menu
+        b2d binary_number
+            Convert a number in unsigned binary form to decimal form
+        d2b decimal_number
+            Convert a number in decimal form to unsigned binary form
+        ipv4_to_class ipv4_address
+            Return the Class of the given IP address
+
+    """)
 
 if __name__ == '__main__':
     ipcalc(argv)
